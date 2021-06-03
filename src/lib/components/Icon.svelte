3a4d18b3c4473533030286cf550f03e9c242fde6
breadthe <omigoshdev@protonmail.com>
2021-06-02 21:17:31 -0500

    Icon component + icons

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Github from '$lib/components/icons/Github.svelte';
+	import Settings from '$lib/components/icons/Settings.svelte';
+	import Twitter from '$lib/components/icons/Twitter.svelte';
+	export let icon, color, title;
+    export let size = 24;
+
+	const icons = [
+        { id: 'github', component: Github },
+        { id: 'settings', component: Settings },
+        { id: 'twitter', component: Twitter },
+    ];
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	fill="rgba(0,0,0,0)"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+    width={size}
+    height={size}
+    {title}
+	class="feather icon {`text-${color}`} {`w-${size}`} {`h-${size}`}"
+>
+	<svelte:component this={icons.find(i => i.id === icon).component} />
+</svg>
