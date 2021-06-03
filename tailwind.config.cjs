7188b3c82ad1bf75638557df876e760d5affc6f6
breadthe <omigoshdev@protonmail.com>
2021-06-02 21:09:52 -0500

    Add Tailwind 0.74 blue + lightBlue

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-	mode: "jit",
-	purge: [
-		"./src/**/*.{html,js,svelte,ts}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    mode: "jit",
+    purge: [
+        "./src/**/*.{html,js,svelte,ts}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "cn-blue-900": "#12283a",
+                blue: colors.lightBlue,
+            },
+        },
+    },
+    plugins: [],
 };
