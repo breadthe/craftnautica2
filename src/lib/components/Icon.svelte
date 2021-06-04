84cbc5b22a476cf01ba3486cfba1b3858136e5a4
breadthe <omigoshdev@protonmail.com>
2021-06-04 17:33:06 -0500

    Icon uses full text-color-value prop + more icons

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -1,13 +1,18 @@
 <script>
+	import Box from '$lib/components/icons/Box.svelte';
 	import Github from '$lib/components/icons/Github.svelte';
 	import Settings from '$lib/components/icons/Settings.svelte';
+	import ShoppingCart from '$lib/components/icons/ShoppingCart.svelte';
 	import Twitter from '$lib/components/icons/Twitter.svelte';
-	export let icon, color, title;
+
+    export let icon, color, title;
     export let size = 24;
 
 	const icons = [
+        { id: 'box', component: Box },
         { id: 'github', component: Github },
         { id: 'settings', component: Settings },
+        { id: 'shopping-cart', component: ShoppingCart },
         { id: 'twitter', component: Twitter },
     ];
 </script>
@@ -23,7 +28,7 @@
     width={size}
     height={size}
     {title}
-	class="feather icon {`text-${color}`} {`w-${size}`} {`h-${size}`}"
+	class="feather icon {color} {`w-${size}`} {`h-${size}`}"
 >
 	<svelte:component this={icons.find(i => i.id === icon).component} />
 </svg>
