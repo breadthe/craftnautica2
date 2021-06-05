d7cbeaeb29dafd6107c221efaafd5f452d4dacf3
breadthe <omigoshdev@protonmail.com>
2021-06-05 07:05:40 -0500

    SubNautica/BelowZero heading component

---
+++ src/lib/components/SubNautica.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let fullDomainName;
+
+	const prefix = fullDomainName === 'Subnautica' ? 'Sub' : 'Below';
+	const suffix = fullDomainName === 'Subnautica' ? 'nautica' : 'Zero';
+</script>
+
+<h1 class="logo flex items-center">
+	<slot />
+	<span class="sub">{prefix}</span><span class="nautica">{suffix}</span>
+</h1>
