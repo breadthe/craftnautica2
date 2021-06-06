f0cfab55a11cd90808aeb8f2023aaaf66663a169
breadthe <omigoshdev@protonmail.com>
2021-06-06 15:53:39 -0500

    Item component complete

---
+++ src/lib/components/Item.svelte
@@ -0,0 +1,76 @@
+<script>
+	import { page } from '$app/stores';
+	import util from '$lib/util';
+	import ItemIcon from '$lib/components/ItemIcon.svelte';
+	import ItemMenu from '$lib/components/ItemMenu.svelte';
+	import { loop_guard } from 'svelte/internal';
+
+	$: domain = $page.path.split('/')[1]; // ['/', 'sn']
+
+	// Array (used to be Object in v1)
+	// Example:
+	// ["Thermoblade", {m: [{c: "Survival_Knife", q: 1}, {c: "Battery", q: 1}], t: "Recipes.Modification_Station.Survival_Knife"}]
+	export let item;
+
+	const id = item[0]; // The item unique identifier
+	let itemRef = `item_${id}`;
+
+	let menu = false; // whether to show the item menu
+
+	function closeItemMenu() {
+		menu = false;
+	}
+
+    function clickOutsideAction(element, callbackFunction) {
+		function onClick(event) {
+			if (!element.contains(event.target)) {
+				callbackFunction();
+			}
+		}
+
+		document.body.addEventListener('click', onClick);
+
+		return {
+			update(newCallbackFunction) {
+				callbackFunction = newCallbackFunction;
+			},
+			destroy() {
+				document.body.removeEventListener('click', onClick);
+			}
+		}
+	}
+</script>
+
+<div class="relative">
+	<div bind:this={itemRef} on:click={() => (menu = true)} use:clickOutsideAction={closeItemMenu} class="item-frame">
+		<ItemIcon {id} />
+
+		<span class="ml-2">
+			{util.pretty(id)}
+		</span>
+	</div>
+
+	{#if menu}
+		<ItemMenu {id} {domain} on:{closeItemMenu} />
+	{/if}
+</div>
+
+<style scoped lang="scss">
+	.item-frame {
+		@apply flex;
+		@apply items-center;
+		@apply m-2;
+		@apply py-2;
+		@apply px-3;
+		@apply cursor-pointer;
+		@apply border;
+		@apply border-blue-600;
+		@apply rounded;
+		@apply text-lg;
+		@apply font-light;
+		&:hover {
+			@apply bg-blue-800;
+			@apply border-blue-600;
+		}
+	}
+</style>
