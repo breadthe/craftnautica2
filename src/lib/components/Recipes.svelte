c00a2934e7a933837c057c1d6a4a47b8a6046fa9
breadthe <omigoshdev@protonmail.com>
2021-06-06 09:27:22 -0500

    Display item types (groups)

---
+++ src/lib/components/Recipes.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { page } from '$app/stores';
+	import { itemsStore } from '../../store';
+	import util from '$lib/util';
+	import Search from '$lib/components/Search.svelte';
+	import SubNautica from '$lib/components/SubNautica.svelte';
+	import Type from '$lib/components/Type.svelte';
+
+	$: domain = $page.path.split('/')[1]; // ['/', 'sn']
+	$: fullDomainName = util.fullDomainName(domain);
+
+    const items = $itemsStore;
+	const types = util.types(items);
+
+	function filterByType(type) {
+		return util.filterByType(items, type);
+	}
+
+	function formatType(type) {
+		return util.formatType(type);
+	}
+</script>
+
+<section class="">
+	<div
+		class="flex flex-col sm:flex-row items-center sm:items-baseline justify-center sm:justify-between mx-4"
+	>
+		<SubNautica {fullDomainName} />
+
+		<Search {fullDomainName} />
+	</div>
+
+	{#each types as type (type)}
+		<Type type={formatType(type)} types={filterByType(type)} />
+	{/each}
+</section>
