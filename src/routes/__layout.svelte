dde154bf475548886903ee2c1bbe4ec4736a0263
breadthe <omigoshdev@protonmail.com>
2021-06-08 00:00:02 -0500

    Initialize the cart when either domain is loaded

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,25 @@
 <script context="module">
-    // Is called once on initial page load
-    export async function load({ page, fetch, session, context }) {
-        const domain = page.path.split('/')[1]; // ['/', 'sn']
+	// Is called once on initial page load
+	export async function load({ page, fetch, session, context }) {
+		const domain = page.path.split('/')[1]; // ['/', 'sn']
 
-        // Load the items into the store, based on domain
-        if (domain === 'sn' || domain === 'bz') {
-            itemsStore.init(domain);
-        }
+		// Load the items into the store, based on domain
+		if (domain === 'sn' || domain === 'bz') {
+			itemsStore.init(domain);
 
-        return {};
-    }
+			// Initialize the cart
+			cart.init();
+
+			// Initialize inventories
+			// TODO
+		}
+
+		return {};
+	}
 </script>
 
 <script>
-    import { itemsStore, cart } from '$store';
+	import { itemsStore, cart } from '$store';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import '../app.scss'; // app.postcss
@@ -22,7 +28,7 @@
 <Header />
 
 <main class="container max-w-6xl mx-auto p-4">
-    <slot />
+	<slot />
 </main>
 
 <Footer />
