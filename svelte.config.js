51d7619758fd82bae086157554433c1ea55befb3
breadthe <omigoshdev@protonmail.com>
2021-06-07 23:03:58 -0500

    Disable SSR ☹️ to allow localStorage to work

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,16 @@
 import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		preprocess({
-			postcss: true
-		}),
-	],
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    preprocess: [
+        preprocess({
+            postcss: true
+        }),
+    ],
+    kit: {
+        ssr: false,
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte',
+    }
 };
 
 export default config;
