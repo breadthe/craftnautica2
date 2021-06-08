149986008945544a16c6bff49157c2db2fe7d885
breadthe <omigoshdev@protonmail.com>
2021-06-07 23:57:47 -0500

    Prep for static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,9 @@
 import path from 'path';
 import preprocess from "svelte-preprocess";
+
+// @see https://github.com/sveltejs/kit/tree/master/packages/adapter-static
+// import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: [
@@ -8,6 +12,9 @@ const config = {
         }),
     ],
     kit: {
+        /* adapter: adapter({
+            fallback: '200.html'
+        }), */
         ssr: false,
         // hydrate the <div id="svelte"> element in src/app.html
         target: '#svelte',
