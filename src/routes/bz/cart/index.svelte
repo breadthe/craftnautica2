f51a28ebe4f68ddef0825aed6192a08720f84138
breadthe <omigoshdev@protonmail.com>
2021-06-08 00:00:22 -0500

    Cart placeholder WIP

---
+++ src/routes/bz/cart/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from '$app/stores';
+	import { cart } from '$store';
+	// import Cart from '$lib/components/Cart.svelte';
+
+	const domain = $page.path.split('/')[1]; // ['/', 'bz']
+</script>
+
+<section>
+	<!-- <Cart /> -->
+</section>
---
+++ src/routes/sn/cart/index.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from '$app/stores';
+	import { cart } from '$store';
+	// import Cart from '$lib/components/Cart.svelte';
+
+	const domain = $page.path.split('/')[1]; // ['/', 'sn']
+</script>
+
+<section>
+	<!-- <Cart /> -->
+</section>
