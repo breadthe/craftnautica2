380bd479f6399095f6f671a67c34350d596f22ad
breadthe <omigoshdev@protonmail.com>
2021-07-22 22:29:00 -0500

    Inventories index

---
+++ src/routes/sn/inventories/index.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+	import util from '$lib/util';
+	import Inventories from '$lib/components/Inventories.svelte';
+
+	const domain = $page.path.split('/')[1]; // ['/', 'sn']
+	const fullDomainName = util.fullDomainName(domain);
+</script>
+
+<section>
+	<Inventories {domain} {fullDomainName} />
+</section>
