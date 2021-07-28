68cbf61fc8e57097f163f11cd95bf65b70f3f081
breadthe <omigoshdev@protonmail.com>
2021-07-27 21:30:21 -0500

    Settings > Export data

---
+++ src/lib/components/ExportData.svelte
@@ -0,0 +1,43 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+
+	let filename;
+	$: {
+		const date = new Date();
+		const now = `${date.getFullYear()}-${
+			date.getMonth() + 1
+		}-${date.getDate()}T${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
+
+		filename = `craftnautica-data_${now}.json`;
+	}
+
+	function exportData() {
+		const exportStr = JSON.stringify({
+			cart: localStorage.getItem('cart'),
+			inventory: localStorage.getItem('inventory')
+		});
+		const dataUri = `data:application/json;charset=utf-8,${encodeURIComponent(exportStr)}`;
+
+		const link = document.createElement('a');
+		link.href = dataUri;
+		link.style = 'visibility:hidden';
+		link.download = filename;
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	}
+</script>
+
+<section class="border-b border-grey-darkest flex flex-col sm:flex-row my-8 pb-8">
+	<h2 class="w-full sm:w-1/6 text-2xl font-bold">Export</h2>
+
+	<div class="w-full sm:w-5/6">
+		<p class="mb-4 mt-4 sm:mt-0">
+			You can export your <strong>Cart</strong> and <strong>Inventory</strong> in JSON format and then
+			restore them from the saved backup.
+		</p>
+		<p class="mb-4">The file is saved to your default Downloads directory.</p>
+
+		<Button icon="download" on:click={() => exportData()}>Export</Button>
+	</div>
+</section>
--- src/routes/settings/index.svelte
+++ src/routes/settings/index.svelte
@@ -1,6 +1,16 @@
 <script>
+	import Icon from '$lib/components/Icon.svelte';
+	import ExportData from '$lib/components/ExportData.svelte';
+	// import ImportData from '$lib/components/ImportData.svelte';
 </script>
 
-<section>
-    Settings
+<section class="max-w-4xl mx-auto">
+	<h1 class="flex items-center">
+		<Icon icon="settings" size="40" color="text-blue-600" title="Settings" klass="mr-4" />
+		Settings
+	</h1>
+
+	<ExportData />
+
+	<!-- <ImportData /> -->
 </section>
